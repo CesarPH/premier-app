224a14355533739b7983baa433d5bd0900adc194
César Palazuelos Higuera <cesar_96_ph@hotmail.com>
2017-11-27 15:11:18 -0600

    Change the name of the the method getTeam to connect
    The reason for this is that instead of having multiple methods, one for each type of request we would have a method that does the request by passing the entire url as a parameter.

--- src/main/java/model/FootballAPI.java
+++ src/main/java/model/FootballAPI.java
@@ -11,13 +11,14 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-// import org.json;
+
 
 
 public class FootballAPI {
     private static FootballAPI instance = new FootballAPI();
+    private static HttpURLConnection conn = null;
+    public static final String URL = "http://api.football-data.org/v1";
     private String apiKey;
-
 
     private FootballAPI(){
         this.apiKey = FootballApiKey.getFootballApiKey();
@@ -27,10 +28,10 @@ public class FootballAPI {
         return instance;
     }
 
-    public JSONObject getTeam(byte id) {
-        HttpURLConnection conn = null;
+    public JSONObject connect(String urlRequest) {
+
         try {
-            URL url = new URL("http://api.football-data.org/v1/teams/" + id);
+            URL url = new URL(urlRequest);
             conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Content-Type", "application/JSON");
@@ -40,30 +41,27 @@ public class FootballAPI {
             conn.setDoOutput(true);
             conn.setDoInput(true);
 
-
             InputStream is = conn.getInputStream();
             BufferedReader rd = new BufferedReader(new InputStreamReader(is));
             StringBuffer response = new StringBuffer();
             String line;
             while ((line = rd.readLine()) != null) {
                 response.append(line);
-                response.append('\r');
             }
             rd.close();
 
-            //JSONParser JSONResponse = new JSONParser();
             JSONObject TeamJSON = new JSONObject(response.toString());
-            return TeamJSON;
 
+            return TeamJSON;
         }catch (MalformedURLException e){
             // new URL() failed
             // ...
-            System.out.println("The URL Failed! \n"+e.getMessage());
+            System.out.println("The URL Failed! \n" + e.getMessage());
 
         }catch (IOException e) {
             // openConnection() failed
             // ...
-            System.out.println("The Connection Failed! \n"+e.getMessage());
+            System.out.println("The Connection Failed! \n" + e.getMessage());
         }
 
         return null;
