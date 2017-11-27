49aba3058b67daef2c36adea813e697cebfc567c
César Palazuelos Higuera <cesar_96_ph@hotmail.com>
2017-11-27 15:22:19 -0600

    Typo errors

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,27 @@
-import model.FootballAPI;
-import org.json.JSONObject;
+
+import model.teamFactory.Team;
+import model.teamFactory.TeamFactory;
 
 public class App {
 
     public static void main(String[] args) {
-        FootballAPI api = FootballAPI.getInstance();
-        JSONObject res = api.getTeam((byte) 66);
-        System.out.println(res.getString("name"));
+        Team team = TeamFactory.createTeam(TeamFactory.MANCHESTER_UNITED);
+
+        System.out.println(team.getFixtures().getAll()[12].getStatus());
+        System.out.println(team.getPlayers());
+
+        team = TeamFactory.createTeam(TeamFactory.MANCHESTER_CITY);
+        System.out.println(team.getPlayers());
+
+        String format = "|%1$-10s|%2$-10s|%3$-20s|\n";
+        System.out.format(format, "A", "AA", "AAA");
+        System.out.format(format, "B", "", "BBBBB");
+        System.out.format(format, "C", "CCCCC", "CCCCCCCC");
+
+        String ex[] = { "E", "EEEEEEEEEE", "E" };
+
+        System.out.format(String.format(format, (Object[]) ex));
+
+
     }
 }
