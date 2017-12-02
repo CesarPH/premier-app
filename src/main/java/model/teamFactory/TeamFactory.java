bd0357bc4bd6125552c91ef1b3b371ca295182d1
César Palazuelos Higuera <cesar_96_ph@hotmail.com>
2017-12-01 20:11:19 -0600

    Add a new static method to use the new constructor on teams

--- src/main/java/model/teamFactory/TeamFactory.java
+++ src/main/java/model/teamFactory/TeamFactory.java
@@ -1,5 +1,7 @@
 package model.teamFactory;
 
+import org.json.JSONObject;
+
 public abstract class TeamFactory {
 
     public static final int MANCHESTER_UNITED = 66;
@@ -12,6 +14,18 @@ public abstract class TeamFactory {
                 return new ManchesterUnited();
             case MANCHESTER_CITY:
                 return new ManchesterCity();
+        }
+
+        return null;
+    }
+
+    public static Team createTeamForTable(int team, JSONObject teamInfo) {
+
+        switch(team) {
+            case MANCHESTER_UNITED:
+                return new ManchesterUnited(teamInfo);
+            case MANCHESTER_CITY:
+                return new ManchesterCity(teamInfo);
         }
 
         return null;
