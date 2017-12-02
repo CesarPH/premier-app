c9b948ef85595665fb0d32781323910a01575d69
César Palazuelos Higuera <cesar_96_ph@hotmail.com>
2017-12-02 15:13:57 -0600

    Add fixtures to Team info in view/controller

--- src/main/java/controller/TeamController.java
+++ src/main/java/controller/TeamController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.teamFactory.Team;
+import model.teamFactory.fixtures.Fixture;
 import model.teamFactory.fixtures.Fixtures;
 import view.TeamView;
 
@@ -14,11 +15,22 @@ public class TeamController {
         this.teamView = teamView;
     }
 
-
-
     public void updateView() {
         teamView.header(teamModel.getName(),teamModel.getCode(), teamModel.getShortName());
-        
+        Fixtures fixtures = teamModel.getFixtures();
+
+        teamView.title("Próximo/actual partido:");
+        teamView.addFixture(fixtures.getNextGame().getHomeTeamName(), fixtures.getNextGame().getAwayTeamName(),
+                fixtures.getNextGame().getGoalsHomeTeam(), fixtures.getNextGame().getGoalsAwayTeam());
+
+        teamView.title("Partidos jugados:");
+        for (Fixture fixture : fixtures.getAll()) {
+            if (fixture.getStatus().equalsIgnoreCase("finished")) {
+                teamView.addFixture(fixture.getHomeTeamName(), fixture.getAwayTeamName(), fixture.getGoalsHomeTeam(), fixture.getGoalsAwayTeam());
+            } else {
+                break;
+            }
+        }
     }
 
 
--- src/main/java/view/TeamView.java
+++ src/main/java/view/TeamView.java
@@ -3,6 +3,20 @@ package view;
 public class TeamView {
     public void header(String name, String code, String shortName) {
         System.out.printf("%s(%s)  \n", name, code);
-        System.out.printf("Nombre abrevidado: %s\n", shortName);
+        System.out.printf("Nombre abrevidado: %s\n\n", shortName);
+
+    }
+
+    public void title(String title) {
+        System.out.println(title);
+    }
+
+    public void addFixture(String home, String away, Object goalsHome, Object goalsAway) {
+        String format = "%1$-25s %2$-3s-  %3$-3s %4$-25s\n\n";
+        if (!goalsAway.equals(null)) {
+            System.out.format(format, home, goalsHome, goalsAway, away);
+        }else {
+            System.out.format(format, home, "", "", away);
+        }
     }
 }
