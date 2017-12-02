1c21b8db3e21d000c93b047b124c6c6410f661c1
César Palazuelos Higuera <cesar_96_ph@hotmail.com>
2017-12-01 22:35:33 -0600

    Add all the teams from the Premier League to TeamFactory

---
+++ src/main/java/model/teamFactory/SwanseCity.java
@@ -0,0 +1,104 @@
+package model.teamFactory;
+
+import model.FootballAPI;
+import model.teamFactory.fixtures.Fixtures;
+import org.json.JSONObject;
+
+public class SwanseCity implements Team {
+
+    private String name;
+    private String code;
+    private String shortName;
+    private int position;
+    private int playedGames;
+    private int goalsFor;
+    private int goalsAgainst;
+    private int wins;
+    private int draws;
+    private int losses;
+
+    private FootballAPI api = FootballAPI.getInstance();
+    private String url = FootballAPI.URL + "/teams/72/";
+
+    public SwanseCity() {
+        JSONObject res = api.connect(url);
+        this.name = res.getString("name");
+        this.code = res.getString("code");
+        this.shortName = res.getString("shortName");
+
+    }
+
+    public SwanseCity(JSONObject team){
+        this.name = team.getString("teamName");
+        this.position = team.getInt("position");
+        this.playedGames = team.getInt("playedGames");
+        this.goalsFor = team.getInt("goals");
+        this.goalsAgainst = team.getInt("goalsAgainst");
+        this.wins = team.getInt("wins");
+        this.draws = team.getInt("draws");
+        this.losses = team.getInt("losses");
+    }
+
+
+    @Override
+    public Fixtures getFixtures() {
+        JSONObject res = api.connect(url + "fixtures");
+        Fixtures fixtures = new Fixtures(res);
+        return fixtures;
+    }
+
+    @Override
+    public JSONObject getPlayers() {
+        return api.connect(url + "players");
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getShortName() {
+        return shortName;
+    }
+
+    @Override
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public int getPlayedGames() {
+        return playedGames;
+    }
+
+    @Override
+    public int getGoalsFor() {
+        return goalsFor;
+    }
+
+    @Override
+    public int getGoalsAgainst() {
+        return goalsAgainst;
+    }
+
+    @Override
+    public int getWins() {
+        return wins;
+    }
+
+    @Override
+    public int getDraws() {
+        return draws;
+    }
+
+    @Override
+    public int getLosses() {
+        return losses;
+    }
+}
